cb3bbba9b0f1e4fd274b2a8e614d2826af8178c7
cjfranzini <cjfranzini@gmail.com>
2017-03-06 11:44:26 -0500

    ad_py_csv: Scripted function

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,22 @@
-PLACE YOUR CODE HERE
+### Q5 ###
+
+import csv
+
+def emails_csv():
+    '''Reads a csv file of the Biostats Faculty at the University of Pennsylvania and writes a list of the faculty email addresses to a new csv file (emails.csv)
+    '''
+    
+    with open('faculty.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        
+        email_list = []
+      
+        for row in reader:
+            if row[' email'] not in email_list: # check for duplicates
+                email_list.append(row[' email'])
+    
+    with open('emails.csv', 'w') as out:
+        writer = csv.writer(out, delimiter = '\n')
+        writer.writerow([email for email in email_list])
+
+emails_csv()

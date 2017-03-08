f75189a8e49f8b1c308a8e08dc1189922a86619a
cjfranzini <cjfranzini@gmail.com>
2017-03-08 17:47:58 -0500

    matrix_algebra: Added print statements

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -14,41 +14,41 @@ w = Matrix([[1],[8],[0],[5]])
 # 1. Matrix Dimensions
 
 # 1.1) A
-A.shape # (2,3)
+print A.shape # (2,3)
 
 # 1.2) B
-B.shape # (2,2)
+print B.shape # (2,2)
 
 # 1.3) C
-C.shape # (3,2)
+print C.shape # (3,2)
 
 # 1.4) D
-D.shape # (2,3)
+print D.shape # (2,3)
 
 # 1.5) u
-u.shape # (1,4)
+print u.shape # (1,4)
 
 # 1.6) w
-w.shape # (4,1)
+print w.shape # (4,1)
  
     
 # 2. Vector Operations
 
 #2.1) u + v
-u + v # [[9, 7, -4, 9]]
+print u + v # [[9, 7, -4, 9]]
 
 #2.2) u - v
-u - v # [[3, -3, -2, 1]]
+print u - v # [[3, -3, -2, 1]]
 
 #2.3) a * u, where a = 6
 a = 6
-a * u # [[36, 12, -18, 30]]
+print a * u # [[36, 12, -18, 30]]
 
 #2.4) u dot v 
-u.dot(v) # 51
+print u.dot(v) # 51
 
 #2.5) norm u
-u.norm() # sqrt(74)
+print u.norm() # sqrt(74)
 
 
 # 3. Matrix Operations
@@ -57,13 +57,13 @@ u.norm() # sqrt(74)
 # Not defined
 
 # 3.2) A - C.T
-A - C.T # [[-4, -7, -3],[3, 6, 4]]
+print A - C.T # [[-4, -7, -3],[3, 6, 4]]
 
 # 3.3) C.T + 3*D
-C.T + 3*D # [[14, 3, 3],[2, 7, 9]]
+print C.T + 3*D # [[14, 3, 3],[2, 7, 9]]
 
 # 3.4) B * A
-B * A # [[-1, -5, -1],[2, 7, 4]]
+print B * A # [[-1, -5, -1],[2, 7, 4]]
 
 # 3.5) B * A.T
 # Not defined
@@ -72,13 +72,13 @@ B * A # [[-1, -5, -1],[2, 7, 4]]
 # Not defined
 
 # 3.7) C * B
-C * B # [[5, -6], [9, -8], [6, -6]]
+print C * B # [[5, -6], [9, -8], [6, -6]]
 
 # 3.8) B**4
-B**4 # [[1, -4],[0, 1]]
+print B**4 # [[1, -4],[0, 1]]
 
 # 3.9) A * A.T
-A * A.T # [[14, 28],[28, 69]]
+print A * A.T # [[14, 28],[28, 69]]
 
 # 3.10) D.T * D
-D.T * D # [[10, -4, 0],[-4, 8, 8],[0, 8, 10]]
+print D.T * D # [[10, -4, 0],[-4, 8, 8],[0, 8, 10]]
